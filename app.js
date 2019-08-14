f4bfa56b2d32e13758a8f1faadc68f461a81f60f
wangex <wangex@think-land.com>
2019-08-14 16:30:25 +0800

    博客后台

--- app.js
+++ app.js
@@ -6,8 +6,34 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
+const path = require('path')
+const fs = require('fs')
+const morgan = require('koa-morgan')
+
 const index = require('./routes/index')
-const users = require('./routes/users')
+const blog = require('./routes/blog')
+const user = require('./routes/user')
+
+const { REDIS_CONF } = require('./conf/db')
+
+// 记录日志
+const ENV = process.env.NODE_ENV;
+if(ENV !== 'production') {
+  // 开发环境
+  app.use(morgan('dev'));
+}else {
+  // 线上环境
+  const logFileName = path.join(__dirname, 'logs', 'access.log');
+  const writeStream = fs.createWriteStream(logFileName, {
+    flags: 'a'
+  })
+  app.use(morgan('combined', {
+    stream: writeStream
+  }))
+}
+
 
 // error handler
 onerror(app)
@@ -16,6 +42,25 @@ onerror(app)
 app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
 }))
+
+// 跨域设置 
+// app.use(convert(cors));
+app.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  await next();
+});
+
+app.use(async (ctx, next)=> {
+ctx.set('Access-Control-Allow-Origin', 'http://localhost:8001');
+ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+if (ctx.method == 'OPTIONS') {
+  ctx.body = 200; 
+} else {
+  await next();
+}
+});
+
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
@@ -32,9 +77,26 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
+// session 配置
+app.keys = ['&jsd_4555%#']
+app.use(session({
+  // 配置cookie
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
+  },
+  // 配置 redis
+  store: redisStore({
+    // all: '127.0.0.1:6379'  // 先写死本地的 redis
+    all:`${REDIS_CONF.host}:${REDIS_CONF.port}`
+  })
+}))
+
 // routes
 app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+app.use(blog.routes(), blog.allowedMethods())
+app.use(user.routes(), user.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
---
+++ routes/user.js
@@ -0,0 +1,33 @@
+const router = require('koa-router')();
+const { login } = require('../controller/user');
+const { SuccessModel, ErrorModel } = require('../model/resModel');
+
+router.prefix('/api/user')
+
+router.post('/login', async function(ctx, next) {
+  console.log(ctx.request.body)
+  const { username, password } = ctx.request.body;
+  const data = await login(username, password);
+  if(data.username) {
+    // 设置 session
+    ctx.session.username = data.username;
+    ctx.session.realname = data.realname;
+    ctx.body = new SuccessModel('登录成功')
+    return;
+  }
+  ctx.body = new ErrorModel('登录失败')
+})
+
+router.get('/session-test', async function (ctx, next) {
+  if(ctx.session.viewCount == null) {
+    ctx.session.viewCount = 0
+  }
+  ctx.session.viewCount++
+
+  ctx.body = {
+    errno: 0,
+    viewCount:ctx.session.viewCount
+  }
+})
+
+module.exports = router;
